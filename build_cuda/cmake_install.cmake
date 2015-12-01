f47087b50faf44e60d6d48ca1ba90973847baae8
Daryl Van Dyke <daryl_van_dyke@fws.gov>
2015-12-01 10:16:15 -0800

    regen sln

--- build_cuda/cmake_install.cmake
+++ build_cuda/cmake_install.cmake
@@ -35,5 +35,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/dkvandyke/Source/Repos/18_OpenSFM/OpenSfM/opensfm/src/build_cuda/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/dkvandyke/Source/Repos/18_OpenSFM/OpenSfM/build_cuda/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
